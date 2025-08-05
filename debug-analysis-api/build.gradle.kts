8701bf3a7b0fca56644dc7b6f3983d495a18474e
单子亨 <shanziheng1721@gmail.com>
2025-08-06 00:06:28 +0100

    Can now work without verifying dependencies

--- debug-analysis-api/build.gradle.kts
+++ debug-analysis-api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization")
     application
 }
 
@@ -11,6 +12,13 @@ repositories {
 }
 
 dependencies {
+    implementation("it.unimi.dsi:fastutil:8.5.16")
+    implementation("org.codehaus.woodstox:stax2-api:4.2.2")
+    implementation("com.fasterxml.woodstox:woodstox-core:7.1.1")
+    implementation("com.fasterxml:aalto-xml:1.3.1")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.0")
+
     implementation(kotlin("stdlib"))
 
     implementation(project(":analysis:analysis-api"))
